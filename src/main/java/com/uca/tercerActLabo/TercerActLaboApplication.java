f09dc67bcd11dc60f0af48c65c0c04864078a9b2
Antonio <00267016@uca.edu.sv>
2020-05-05 20:19:19 -0600

    project added

---
+++ src/main/java/com/uca/tercerActLabo/TercerActLaboApplication.java
@@ -0,0 +1,13 @@
+package com.uca.tercerActLabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TercerActLaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TercerActLaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/tercerActLabo/TercerActLaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.tercerActLabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TercerActLaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
